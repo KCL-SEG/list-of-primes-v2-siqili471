0e361cf298ea7e74b9245ae7d16dc0f8b5354a27
Siqi Li <1114673087@qq.com>
2023-10-01 11:59:28 +0100

    complete the primes funtion

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    count = 2
+    if number_of_primes > 0:
+        while number_of_primes > 0:
+            if is_prime(count):
+                list.append(count)
+                number_of_primes -= 1
+            count += 1
+    else:
+        raise ValueError
     return list
+
+def is_prime(num):
+    for i in range(2,num//2+1):
+        if num % i == 0:
+            return False
+    return True
